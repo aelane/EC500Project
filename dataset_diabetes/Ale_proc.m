48f5d58e4cd300bc857ba4d8c23a3d62602f79bc
Alejandro Eguren <aeguren@bu.edu>
2015-12-06 20:52:22 -0500

    The data has finally been processed. The final matrix is saved as Final_Ale.mat and I’ve also included some notes on the processing in the text file.

--- dataset_diabetes/Ale_proc.m
+++ dataset_diabetes/Ale_proc.m
@@ -1,12 +1,25 @@
 %Time to process that data
 
-col = 18;
+col = 45;
 for i = 1:length(T)
+    
     if ischar(T{i,col})
         fprintf('%s\n',T{i,col});
-        if strcmp(T{i,col},'>200')
-            T{i,col} = 200;
+        
+        if strcmp(T{i,col},'<30')
+                T{i,col} = -1;
         end
         
+%           if strcmp(T{i,j},'Up')
+%               T{i,j} = 2;
+%           
+%           elseif strcmp(T{i,j},'Down')
+%               T{i,j} = -2;
+%     
+%           elseif strcmp(T{i,j},'Steady')
+%               T{i,j} = 1;
+%           end
+        
     end
+    
 end
