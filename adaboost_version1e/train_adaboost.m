4256c776a168d1f841cdead04ab5cb5b4fe5194d
Annie Lane <annieel94@gmail.com>
2015-12-09 18:51:43 -0500

    Completed modified versions of Adaboost
    Created separate train_adaboost and test_adaboost functions with
    comments

--- adaboost_version1e/train_adaboost.m
+++ adaboost_version1e/train_adaboost.m
@@ -1,4 +1,4 @@
-function model= train_adaboost(feature_matrix, class_labels, num_iter)
+function model= train_adaboost(feature_matrix, class_labels, num_t)
 % Modified from Copyright (c) 2010, Dirk-Jan Kroon. All rights reserved.
 % Function is written by D.Kroon University of Twente (August 2010)
 % See license.txt
@@ -26,7 +26,15 @@ function model= train_adaboost(feature_matrix, class_labels, num_iter)
 %   num_iter: number of weak classifiers to be use for training
 
 % Output:
-%   model: struct with weak classifiers
+%   model: struct with num_t weak classifiers; each weak classifier has:
+%       alpha - the weight of the classifier
+%       feature - the feature used to make the decision
+%       threshold - the threshold value on the feature to split the data
+%       direction - which side of the threshold is which class
+%       boundary -
+%       error - the error with this classifier
+%       poserror - the percent of actually positive examples misclassified
+%       negerror - the percent of actually negative examples misclassified
 
 %% Initialize Variables
 
@@ -35,55 +43,70 @@ model=struct;
 
 % Number of examples
 num_examples = length(class_labels);
+ind_positive = find(class_labels == 1);
+ind_negative = find(class_labels == -1);
+num_positive = length(ind_positive);
+num_negative = length(ind_negative);
 
 % Initialize all examples to the same training weight
-train_weight=ones(num_examples,1)/num_examples;
+sample_weight=ones(num_examples,1)/num_examples;
 
 % This variable will contain the results of the single weak
 % classifiers weight by their alpha
-% Initialize vector to store the predicted result of current weak classifier in a vector
-weak_predict = zeros(num_examples, 1); %Previously estimateclassnum
+% Initialize vector weak_predict_sum to store the predicted result of all weak classifiers
+% so far -- the sum of the classifier times the classifier weight
+predict_sum = zeros(num_examples, 1); %Previously estimateclassnum
 
-% Calculate max min of the data
-% Calculate the range of each feature in the data set
-range_features = [min(feature_matrix); max(feature_matrix)]; %Previously boundary
-boundary=[min(datafeatures) max(datafeatures,[],1)];
-% Do all model training itterations
-for t=1:num_iter
+% Calculate the range (min and max) of each feature in the data set
+min_feature_vals = min(feature_matrix);
+max_feature_vals = max(feature_matrix);
+%boundary=[min(datafeatures) max(datafeatures,[],1)];
+%% Do all model training iterations
+for t=1:num_t
     % Find the best threshold to separate the data in two classes
-    [estimateclass,error,h] = WeightedThresholdClassifier(feature_matrix, class_labels, train_weight);
+    [weak_predict, error, weak_classifier] = WeightedThresholdClassifier(feature_matrix, class_labels, sample_weight);
     
-    % Weak classifier influence on total result is based on the current
-    % classification error
-    alpha=1/2 * log((1-error)/max(error,eps));
+    % Alpha is the weight for the weak classifier, based on classification
+    % error (where eps is the floating-point relative accuracy - avoid
+    % dividing by zero)
+    alpha= 1/2 * log((1-error)/max(error,eps));
     
     % Store the model parameters
     model(t).alpha = alpha;
-    model(t).dimension=h.dimension;
-    model(t).threshold=h.threshold;
-    model(t).direction=h.direction;
-    model(t).boundary = boundary;
+    model(t).feature=weak_classifier.feature;
+    model(t).threshold=weak_classifier.threshold;
+    model(t).direction=weak_classifier.direction;
+    %model(t).boundary = boundary;
     % We update D so that wrongly classified samples will have more weight
-    train_weight = train_weight.* exp(-model(t).alpha.*dataclass.*estimateclass);
-    train_weight = train_weight./sum(train_weight);
+    % in next iteration
+    sample_weight = sample_weight .* exp(-alpha * class_labels .* weak_predict);
+    sample_weight = sample_weight./sum(sample_weight); %Normalize the weights
     
-    % Calculate the current error of the cascade of weak
-    % classifiers
-    estimateclasssum=estimateclasssum +estimateclass*model(t).alpha;
-    estimateclasstotal=sign(estimateclasssum);
-    model(t).error=sum(estimateclasstotal~=dataclass)/length(dataclass);
-    if(model(t).error==0), break; end
+    % Update the predict_sum and determine the current cascade of
+    % classifiers prediction
+    predict_sum = predict_sum + weak_predict * alpha;
+    curr_predict_label = sign(predict_sum);
+    % Calculate the current error of the cascade of weak classifiers
+    model(t).error = sum(curr_predict_label ~= class_labels)/num_examples;
+    
+    % Calculate the misclassification error for the positive and negative examples
+    model(t).poserror = sum(curr_predict_label(ind_positive) ~= 1)/num_positive;
+    model(t).negerror = sum(curr_predict_label(ind_negative) ~= -1)/num_negative;
+    % If the error is 0, then break out of loop
+    if(model(t).error==0) 
+        break; 
+    end
 end
 
 
 function [estimateclass,err,h] = WeightedThresholdClassifier(datafeatures,dataclass,dataweight)
-% This is an example of an "Weak Classifier", it caculates the optimal
+% This is an example of an "Weak Classifier", it calculates the optimal
 % threshold for all data feature dimensions.
-% It then selects the dimension and  threshold which divides the
+% It then selects the dimension and  treshold which divides the 
 % data into two class with the smallest error.
 
-% Number of threshold steps
-num_threshold_steps=200000;
+% Number of treshold steps
+ntre=2e5;
 
 % Split the data in two classes 1 and -1
 r1=datafeatures(dataclass<0,:); 
@@ -96,14 +119,17 @@ minr=min(datafeatures,[],1)-1e-10;
 maxr=max(datafeatures,[],1)+1e-10;
 
 % Make a weighted histogram of the two classes
-p2c= ceil((bsxfun(@rdivide,bsxfun(@minus,r2,minr),(maxr-minr)))*(num_threshold_steps-1)+1+1e-9);   p2c(p2c>num_threshold_steps)=num_threshold_steps;
-p1f=floor((bsxfun(@rdivide,bsxfun(@minus,r1,minr),(maxr-minr)))*(num_threshold_steps-1)+1-1e-9);  p1f(p1f<1)=1;
+p2c= ceil((bsxfun(@rdivide,bsxfun(@minus,r2,minr),(maxr-minr)))*(ntre-1)+1+1e-9);   
+p2c(p2c>ntre)=ntre;
+p1f=floor((bsxfun(@rdivide,bsxfun(@minus,r1,minr),(maxr-minr)))*(ntre-1)+1-1e-9);  
+p1f(p1f<1)=1;
 ndims=size(datafeatures,2);
-i1=repmat(1:ndims,size(p1f,1),1);  i2=repmat(1:ndims,size(p2c,1),1);
-h1f=accumarray([p1f(:) i1(:)],repmat(w1(:),ndims,1),[num_threshold_steps ndims],[],0);
-h2c=accumarray([p2c(:) i2(:)],repmat(w2(:),ndims,1),[num_threshold_steps ndims],[],0);
+i1=repmat(1:ndims,size(p1f,1),1);  
+i2=repmat(1:ndims,size(p2c,1),1);
+h1f=accumarray([p1f(:) i1(:)],repmat(w1(:),ndims,1),[ntre ndims],[],0);
+h2c=accumarray([p2c(:) i2(:)],repmat(w2(:),ndims,1),[ntre ndims],[],0);
 
-% This function calculates the error for every all possible treshold value
+% This function calculates the error for every all possible threshold value
 % and dimension
 h2ic=cumsum(h2c,1);
 h1rf=cumsum(h1f(end:-1:1,:),1); h1rf=h1rf(end:-1:1,:);
@@ -111,28 +137,24 @@ e1a=h1rf+h2ic;
 e2a=sum(dataweight)-e1a;
 
 % We want the treshold value and dimension with the minimum error
-[err1a,ind1a]=min(e1a,[],1);  dim1a=(1:ndims); dir1a=ones(1,ndims);
-[err2a,ind2a]=min(e2a,[],1);  dim2a=(1:ndims); dir2a=-ones(1,ndims);
+[err1a,ind1a]=min(e1a,[],1);  
+dim1a=(1:ndims); 
+dir1a=ones(1,ndims);
+[err2a,ind2a]=min(e2a,[],1);  
+dim2a=(1:ndims); 
+dir2a=-ones(1,ndims);
 A=[err1a(:),dim1a(:),dir1a(:),ind1a(:);err2a(:),dim2a(:),dir2a(:),ind2a(:)];
-[err,i]=min(A(:,1)); dim=A(i,2); dir=A(i,3); ind=A(i,4);
-thresholds = linspace(minr(dim),maxr(dim),num_threshold_steps);
+[err,i]=min(A(:,1)); 
+dim=A(i,2); dir=A(i,3); ind=A(i,4);
+thresholds = linspace(minr(dim),maxr(dim),ntre);
 thr=thresholds(ind);
 
 % Apply the new treshold
-h.dimension = dim;
-h.threshold = thr;
+h.feature = dim; 
+h.threshold = thr; 
 h.direction = dir;
-estimateclass=ApplyClassTreshold(h,datafeatures);
+estimateclass=ApplyClassThreshold(h,datafeatures);
 
-function y = ApplyClassTreshold(h, x)
-% Draw a line in one dimension (like horizontal or vertical)
-% and classify everything below the line to one of the 2 classes
-% and everything above the line to the other class.
-if(h.direction == 1)
-    y =  double(x(:,h.dimension) >= h.threshold);
-else
-    y =  double(x(:,h.dimension) < h.threshold);
-end
-y(y==0) = -1;
+
 
 
